15e5318a86a06f003c83d900081d5982889d11fd
Tony Minott <minottanthonya@gmail.com>
2017-01-31 11:09:15 -0500

    Finished module 10

--- server/app.js
+++ server/app.js
@@ -13,7 +13,11 @@ User.sync(); // User({ force: true }); //drops the table compeletly (line 27ish)
 // The app starts!
 app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+
 app.use(require('./middleware/headers'));
+
+app.use('api/user', require('./routes/user'));
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
@@ -23,27 +27,3 @@ app.listen(3000, function(){
 	console.log("app is listening on port 3000");
 });
 
-app.post('/api/user', function(req, res){
-	//when we post to api user, it will want a user object in the body
-	var username = req.body.user.username;
-	var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
-
-	//Match the model we create above
-	//Sequelize - take the user model and go out to the db and create this:
-	User.create({
-		username: username,
-		passwordhash: ''
-}).then(
-		//Sequelize is going to return the object it created from db.
-		function createSuccess(user){
-			//successful get thsi:
-			res.json({
-				user: user,
-				message: 'create'
-			});
-		},
-		function createError(err){
-			res.send(500, err.message);
-		}
-	);
-});
--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,8 @@
 // talks to the table user
 
 module.exports = function(sequelize, DataTypes){
-	var User = sequelize.define('user,'{
-	username: Sequelize.STRING,
-	passwordhas: Sequelize.STRING
-		});
-		return User;
+	return sequelize.define('user,' {
+		username: Sequelize.STRING,
+		passwordhas: Sequelize.STRING
+	});
 };
