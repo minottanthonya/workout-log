75833ca6b801ebefcf37dcc550ebcd1a9b7070a1
Tony Minott <minottanthonya@gmail.com>
2017-02-01 16:37:57 -0500

    Potential finished module 17

--- server/app.js
+++ server/app.js
@@ -24,6 +24,9 @@ app.use('/api/user', require('./routes/user'));
 //login route
 app.use('/api/login', require('./routes/session'));
 
+//definition route
+app.use('/api/definition', require('./routes/definition'));
+
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
---
+++ server/routes/definition.js
@@ -0,0 +1,62 @@
+
+var router = require('express').Router();
+var sequelize = require('../db');
+var User = sequelize.import('../models/user');
+var Definition = sequelize.import('../models/definition');
+
+router.post('/', function(req, res){
+
+	//variables
+	var description =req.body.definition.desc;
+	var logType=req.body.definition.type;
+	var owner = req.user.id;
+
+	//methods
+	Definition
+	// objects must match the model
+	.create({
+		description: description,
+		logType: logType,
+		owner: owner	
+		})
+
+		.then(
+			// createSuccess function		
+			function createSuccess(definition) {
+			//send a response as json
+			res.json({
+				definition: definition
+			});	
+		},
+			// createError function
+			function createError(err){
+				res.send(500, err.message);
+			}
+
+
+			);
+	});
+
+router.get('/', function(req, res){
+	// user variable
+	var userid = req.user.id;
+	Definition
+	// findAll by owner method
+	.findAll({
+		where:{ owner:userid }
+	})
+	// findAll by owner method
+	.then(
+			// success
+			function findAllSuccess(data){
+				// console.log(data);
+				res.json(data);
+			},
+			// failure
+			function findAllError(err) {
+				res.sent(500, err.message);
+			}			
+	);
+});
+
+module.exports = router;
